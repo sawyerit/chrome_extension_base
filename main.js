debcd8b3c9412704d4058bc08c79aef42b4a8aa3
John Binning <john.binning17@gmail.com>
2017-10-11 15:03:57 -0600

    fix bug where HTML did not display, add css

--- main.js
+++ main.js
@@ -1,9 +1,8 @@
 function getJIRAFeed(callback, errorCallback){
-    var user = document.getElementById("user").value;
+    let user = document.getElementById("user").value;
     if(user == undefined) return;
-    
-    var url = "https://jira.secondlife.com/activity?maxResults=50&streams=user+IS+"+user+"&providers=issues";
-    make_request(url, "").then(function(response) {
+    let url = `https://jira.secondlife.com/activity?maxResults=50&streams=user+IS+${user}&providers=issues`;
+    makeRequest(url, "").then((response) => {
       // empty response type allows the request.responseXML property to be returned in the makeRequest call
       callback(url, response);
     }, errorCallback);
@@ -14,23 +13,23 @@ function getJIRAFeed(callback, errorCallback){
  *   formatted for rendering.
  * @param {function(string)} errorCallback - Called when the query or call fails.
  */
-async function getQueryResults(s, callback, errorCallback) {                                                 
+async function getQueryResults(s, callback, errorCallback) {    
     try {
-      var response = await make_request(s, "json");
+      let response = await makeRequest(s, "json");
       callback(createHTMLElementResult(response));
     } catch (error) {
       errorCallback(error);
     }
 }
 
-function make_request(url, responseType) {
-  return new Promise(function(resolve, reject) {
-    var req = new XMLHttpRequest();
+function makeRequest(url, responseType) {
+  return new Promise((resolve, reject) => {
+    let req = new XMLHttpRequest();
     req.open('GET', url);
     req.responseType = responseType;
 
     req.onload = function() {
-      var response = responseType ? req.response : req.responseXML;
+      let response = responseType ? req.response : req.responseXML;
       if(response && response.errorMessages && response.errorMessages.length > 0){
         reject(response.errorMessages[0]);
         return;
@@ -56,6 +55,7 @@ function make_request(url, responseType) {
 
 
 function loadOptions(){
+  // console.log('loadOptions')
   chrome.storage.sync.get({
     project: 'Sunshine',
     user: 'nyx.linden'
@@ -65,14 +65,15 @@ function loadOptions(){
   });
 }
 function buildJQL(callback) {
-  var callbackBase = "https://jira.secondlife.com/rest/api/2/search?jql=";
-  var project = document.getElementById("project").value;
-  var status = document.getElementById("statusSelect").value;
-  var inStatusFor = document.getElementById("daysPast").value
-  var fullCallbackUrl = callbackBase;
-  fullCallbackUrl += 'project=${project}+and+status=${status}+and+status+changed+to+${status}+before+-${inStatusFor}d&fields=id,status,key,assignee,summary&maxresults=100';
+  let callbackBase = "https://jira.secondlife.com/rest/api/2/search?jql=";
+  let project = document.getElementById("project").value;
+  let status = document.getElementById("statusSelect").value;
+  let inStatusFor = document.getElementById("daysPast").value
+  let fullCallbackUrl = callbackBase;
+  fullCallbackUrl += `project=${project}+and+status=${status}+and+status+changed+to+${status}+before+-${inStatusFor}d&fields=id,status,key,assignee,summary&maxresults=100`;
   callback(fullCallbackUrl);
 }
+
 function createHTMLElementResult(response){
 
 // 
@@ -81,19 +82,38 @@ function createHTMLElementResult(response){
 // hint: you may run the application as well if you fix the bug. 
 // 
 
-  return '<p>There may be results, but you must read the response and display them.</p>';
-  
+  document.getElementById('query-result').hidden = false;
+
+  const queryResults = response.issues.map( (issue) => {
+    const info = `
+      <article class='issue-container'>
+        <h6><strong>ISSUE:</strong> ${issue.fields.summary}</h6>
+        <p><strong>ISSUE ID:</strong> ${issue.id}</p>
+        <p><strong>STATUS:</strong> ${issue.fields.status.name} <img src="${issue.fields.status.iconUrl}"></p>
+        <div><a href=${issue.self}  target="_blank"><strong> MORE INFO:</strong> ${issue.self}</a></div>
+      </article>
+      `;
+    return info;
+  })
+
+  const displayData = `
+    <h4>Total results: ${response.total}</h4>
+    <h5>Issues: </h5>
+    <section>${queryResults.join('')}</section>
+  `;
+
+  return displayData;  
 }
 
 // utility 
 function domify(str){
-  var dom = (new DOMParser()).parseFromString('<!doctype html><body>' + str,'text/html');
+  let dom = (new DOMParser()).parseFromString('<!doctype html><body>' + str,'text/html');
   return dom.body.textContent;
 }
 
-function checkProjectExists(){
+async function checkProjectExists(){
     try {
-      return await make_request("https://jira.secondlife.com/rest/api/2/project/SUN", "json");
+      return await makeRequest("https://jira.secondlife.com/rest/api/2/project/SUN", "json");
     } catch (errorMessage) {
       document.getElementById('status').innerHTML = 'ERROR. ' + errorMessage;
       document.getElementById('status').hidden = false;
@@ -108,6 +128,7 @@ document.addEventListener('DOMContentLoaded', function() {
       loadOptions();
 
       // query click handler
+      
       document.getElementById("query").onclick = function(){
         // build query
         buildJQL(function(url) {
@@ -119,7 +140,7 @@ document.addEventListener('DOMContentLoaded', function() {
             document.getElementById('status').innerHTML = 'Query term: ' + url + '\n';
             document.getElementById('status').hidden = false;
             
-            var jsonResultDiv = document.getElementById('query-result');
+            let jsonResultDiv = document.getElementById('query-result');
             jsonResultDiv.innerHTML = return_val;
             jsonResultDiv.hidden = false;
 
@@ -133,24 +154,24 @@ document.addEventListener('DOMContentLoaded', function() {
       // activity feed click handler
       document.getElementById("feed").onclick = function(){   
         // get the xml feed
-        getJIRAFeed(function(url, xmlDoc) {
+        getJIRAFeed((url, xmlDoc) => {
           document.getElementById('status').innerHTML = 'Activity query: ' + url + '\n';
           document.getElementById('status').hidden = false;
 
           // render result
-          var feed = xmlDoc.getElementsByTagName('feed');
-          var entries = feed[0].getElementsByTagName("entry");
-          var list = document.createElement('ul');
+          let feed = xmlDoc.getElementsByTagName('feed');
+          let entries = feed[0].getElementsByTagName("entry");
+          let list = document.createElement('ul');
 
-          for (var index = 0; index < entries.length; index++) {
-            var html = entries[index].getElementsByTagName("title")[0].innerHTML;
-            var updated = entries[index].getElementsByTagName("updated")[0].innerHTML;
-            var item = document.createElement('li');
+          for (let index = 0; index < entries.length; index++) {
+            let html = entries[index].getElementsByTagName("title")[0].innerHTML;
+            let updated = entries[index].getElementsByTagName("updated")[0].innerHTML;
+            let item = document.createElement('li');
             item.innerHTML = new Date(updated).toLocaleString() + " - " + domify(html);
             list.appendChild(item);
           }
 
-          var feedResultDiv = document.getElementById('query-result');
+          let feedResultDiv = document.getElementById('query-result');
           if(list.childNodes.length > 0){
             feedResultDiv.innerHTML = list.outerHTML;
           } else {
